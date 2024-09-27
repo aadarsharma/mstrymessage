15d4768515eb29a5aa52c15aabf132bb38f4a6f2
aadarshsharma <adarsharma56763@gmail.com>
2024-09-27 16:33:05 +0530

    added src/app/api/send-messages/route.ts

--- src/app/api/send-message/route.ts
+++ src/app/api/send-message/route.ts
@@ -1,6 +1,6 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
-import {Message} from "@/model/User";
+import {Message} from "@/model/User"; 
 
 export async function POST(request: Request) {
     await dbConnect();
